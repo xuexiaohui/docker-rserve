ca90e869e15d3b371526d4ceae90d41575e933c2
Xiaohui Xue <xiaohui.xue@sap.com>
2017-05-03 14:34:23 +0200

    Run RServe using a dedicated user and install RAppArmor

--- install-rserve.R
+++ install-rserve.R
@@ -1,4 +1,6 @@
-install.packages(c('Rserve','psych','devtools','sqldf','MASS','tibble','lazyeval','stringi','magrittr','labeling','lattice','evaluate','highr','markdown','yaml','dplyr','arules','rpart','adabag','randomForest','monmlp','nnet','R.utils','circlize','Lahman','abc','acepack','actuar','ape','aplpack','approximator','arm','bclust','cclust','cluster','coda','coin','corpcor','data.table','digest','dlm','bclust','evaluate','expm','foreach','forecast','gamlr','ggplot2','polynom','reshape','stringr','tidyr','diffobj','tidytext','plotly','janitor','gmodels','openxlsx','choroplethr','RColorBrewer','metricsgraphics','bit64'), repos='http://cran.r-project.org')  
+install.packages(c('Rserve','RAppArmor'), repos='http://cran.r-project.org')  
+
+install.packages(c('psych','devtools','sqldf','MASS','tibble','lazyeval','stringi','magrittr','labeling','lattice','evaluate','highr','markdown','yaml','dplyr','arules','rpart','adabag','randomForest','monmlp','nnet','R.utils','circlize','Lahman','abc','acepack','actuar','ape','aplpack','approximator','arm','bclust','cclust','cluster','coda','coin','corpcor','data.table','digest','dlm','bclust','evaluate','expm','foreach','forecast','gamlr','ggplot2','polynom','reshape','stringr','tidyr','diffobj','tidytext','plotly','janitor','gmodels','openxlsx','choroplethr','RColorBrewer','metricsgraphics','bit64'), repos='http://cran.r-project.org')  
 
 #library(devtools)
 #devtools::install_github("timelyportfolio/rcdimple")
