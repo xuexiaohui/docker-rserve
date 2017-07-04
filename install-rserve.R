d8f4f2ce851d5d0080d81723e43232dd5fd555f6
Xiaohui Xue <xiaohui.xue@sap.com>
2017-07-04 11:01:32 +0200

    New update to allow installing all packages requested by BOC

--- install-rserve.R
+++ install-rserve.R
@@ -1,10 +1,12 @@
 install.packages(c('Rserve','RAppArmor'), repos='http://cran.r-project.org')  
 
-###First list of packages to install (2017-06-13)
-install.packages(c('abc','acepack','actuar','adabag','ape','aplpack','approximator','Arm','arules','assertthat','bclust','bit64','cclust','choroplethr','circlize','CLUSTER','Coda','coin','corpcor','data.table','DBI','diffobj','dlm','dplyr','Expm','forecast-R','gamlr','ggplot2','gmodels','highr','Iterators','Janitor','jsonlite','labeling','Lahman','Lattice','Lazyeval','magrittr','markdown','MASS','metricsgraphics','monmlp','Nnet','openxlsx','plotly','plyr','polynom','Psych','rpart','r-digest','r-evaluate','r-foreach','r-yaml','R.utils','randomForest','RColorBrewer','reshape','rJava','RJDBC','RODBC','sqldf','stringi','stringr','tibble','tidyr','tidytext'), repos='http://cran.r-project.org')  
+#install.packages(c('rJava','RJDBC','RODBC'), repos='http://cran.r-project.org', dependencies=TRUE)  
+
+#First list of packages to install (2017-06-13)
+install.packages(c('abc','acepack','actuar','adabag','ape','aplpack','approximator','arm','arules','assertthat','bclust','bit64','cclust','choroplethr','circlize','CLUSTER','coda','coin','corpcor','data.table','DBI','diffobj','dlm','dplyr','expm','forecast','gamlr','ggplot2','gmodels','highr','Iterators','janitor','jsonlite','labeling','Lahman','lattice','Lazyeval','magrittr','markdown','MASS','metricsgraphics','monmlp','nnet','openxlsx','plotly','plyr','polynom','Psych','rpart','r-digest','r-evaluate','r-foreach','r-yaml','R.utils','randomForest','RColorBrewer','reshape','rJava','RJDBC','RODBC','sqldf','stringi','stringr','tibble','tidyr','tidytext'), repos='http://cran.r-project.org')  
 
 
-### Second list of packages to install (2017-06-30)
+# Second list of packages to install (2017-06-30)
 install.packages(c('devtools','ggvis','googleVis','highcharter','plotly'), repos='http://cran.r-project.org')  
 library(devtools)
 devtools::install_github("ramnathv/rCharts")
