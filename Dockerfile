c3417b8db06faae750f61251f7cfadc2da42779c
Xiaohui Xue <xiaohui.xue@sap.com>
2017-09-25 16:37:50 +0200

    resolve merge conflict

--- Dockerfile
+++ Dockerfile
@@ -60,14 +60,10 @@ RUN apt-get -y clean && \
 	libgmp-dev \	 	
 	default-jre \	  	
 	default-jdk \	  	
-<<<<<<< HEAD
 	libnlopt-dev \
 	libglu1-mesa-dev \
 	freeglut3-dev \
-	mesa-common-dev
-=======
-	libnlopt-dev
->>>>>>> 2bdbb24c4edbfe488486848ae20d5c4fa2fa68f5
+	mesa-common-dev 
 
 ###Update the library libgdal that allows the package choroplethr to be installed 
 RUN add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable && \
--- install-rserve.R
+++ install-rserve.R
@@ -1,21 +1,4 @@
 install.packages(c('Rserve','RAppArmor'), repos='http://cran.r-project.org')  
-<<<<<<< HEAD
-=======
-
-#install.packages(c('rJava','RJDBC','RODBC'), repos='http://cran.r-project.org', dependencies=TRUE)  
-
-#First list of packages to install (2017-06-13)
-install.packages(c('abc','acepack','actuar','adabag','ape','aplpack','approximator','arm','arules','assertthat','bclust','bit64','cclust','choroplethr','circlize','CLUSTER','coda','coin','corpcor','data.table','DBI','diffobj','dlm','dplyr','expm','forecast','gamlr','ggplot2','gmodels','highr','Iterators','janitor','jsonlite','labeling','lattice','Lazyeval','magrittr','markdown','MASS','metricsgraphics','monmlp','nnet','openxlsx','plotly','plyr','polynom','Psych','rpart','r-digest','r-evaluate','r-foreach','r-yaml','R.utils','randomForest','RColorBrewer','reshape','rJava','RJDBC','RODBC','sqldf','stringi','stringr','tibble','tidyr','tidytext'), repos='http://cran.r-project.org')  
-
-
-# Second list of packages to install (2017-06-30)
-install.packages(c('devtools','ggvis','googleVis','highcharter','plotly'), repos='http://cran.r-project.org')  
-library(devtools)
-devtools::install_github("ramnathv/rCharts")
-
->>>>>>> 2bdbb24c4edbfe488486848ae20d5c4fa2fa68f5
-
-#install.packages(c('rJava','RJDBC','RODBC'), repos='http://cran.r-project.org', dependencies=TRUE)  
 
 #First list of packages to install (2017-06-13)
 install.packages(c('abc','acepack','actuar','adabag','ape','aplpack','approximator','arm','arules','assertthat','bclust','bit64','cclust','choroplethr','circlize','CLUSTER','coda','coin','corpcor','data.table','DBI','diffobj','dlm','dplyr','expm','forecast','gamlr','ggplot2','gmodels','highr','Iterators','janitor','jsonlite','labeling','lattice','Lazyeval','magrittr','markdown','MASS','metricsgraphics','monmlp','nnet','openxlsx','plotly','plyr','polynom','Psych','rpart','r-digest','r-evaluate','r-foreach','r-yaml','R.utils','randomForest','RColorBrewer','reshape','rJava','RJDBC','RODBC','sqldf','stringi','stringr','tibble','tidyr','tidytext'), repos='http://cran.r-project.org')  
